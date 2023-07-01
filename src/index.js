46592cfb75e074a8895f9e4b856c41e797544756
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-07-01 13:35:14 +0530

    moved to es module syntax

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const connect= require('./config/database')
-const TweetService = require('./services/tweet-service');
+import express from 'express'
+import connect from './config/database.js'
 const app = express();
 const PORT=3000;
 app.listen(PORT,async()=>{
     console.log(`Server started on port ${PORT}`);
     await connect();
     console.log('MongoDB connected');
-   
-  const service = new TweetService();
-  const tweet = await service.create({
-    content: 'This is #testing for #hashtag #second'
-  })
-  console.log(tweet);
+    // const tweetServ = new TweetService
+    // const tweet = await tweetServ.create({content: 'es module #test'});
+    // console.log(tweet);
+
 
 
 })
