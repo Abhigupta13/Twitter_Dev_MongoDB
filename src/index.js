e9cb66602f1400f96fab7f084864d95bbd29534b
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-07-01 16:00:22 +0530

    Added controller and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import bodyParser from 'body-parser';
 import connect from './config/database.js'
+import apiRoutes from './routes/index.js'
 const app = express();
 const PORT=3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api',apiRoutes)
 app.listen(PORT,async()=>{
     console.log(`Server started on port ${PORT}`);
     await connect();
