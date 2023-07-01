02dc2da0ce4dde3ca48fbfa06dda0788ea43e6b3
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-07-01 13:00:00 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const connect= require('./config/database')
-const Tweet = require('./models/tweet')
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+const TweetService = require('./services/tweet-service');
 const app = express();
 const PORT=3000;
 app.listen(PORT,async()=>{
     console.log(`Server started on port ${PORT}`);
     await connect();
     console.log('MongoDB connected');
-  const tweetRepo = new TweetRepository();
-    // console.log(tweets);
-    // const tweet = await tweetRepo.create({content: "new content with comment 2"});
-    // const comment= await Comment.create({content:"New comment 2"});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-    // const tweet = await tweetRepo.getAll(0,3);
-    const tweet = await tweetRepo.create({content: "with hook "})
-    console.log(tweet);
-    // console.log(tweet[0].contentWithEmail);
+   
+  const service = new TweetService();
+  const tweet = await service.create({
+    content: 'This is #testing for #hashtag #second'
+  })
+  console.log(tweet);
+
 
 })
