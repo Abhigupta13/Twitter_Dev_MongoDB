602372502a484877b1c869dd091fc239a452d05e
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-07-05 10:50:48 +0530

    Added signup with encrypted password

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { createTweet,getTweet } from '../../controller/tweet-controller.js';
 import {toggleLike} from '../../controller/like-controller.js'
 import { createComment } from '../../controller/comment-controller.js';
+import { signup } from '../../controller/auth-controller.js';
 const router = express.Router();
 
 router.post('/tweets', createTweet);
@@ -10,6 +11,7 @@ router.get('/tweets/:id', getTweet);
 
 router.post('/likes/toggle',toggleLike);
 router.post('/comments',createComment);
+router.post('/signup',signup);
 
 
 export default router;
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,8 @@
 import TweetService from './tweet-service.js'
 import LikeService from './like-service.js'
+import UserService from './user-service.js'
 export{
     TweetService ,
-    LikeService
+    LikeService,
+    UserService
 }
